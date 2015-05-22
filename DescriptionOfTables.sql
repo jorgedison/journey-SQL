19eaaadad6bb0537bd374403c70065f89300589f
Jorge Rodriguez Flores <jrodriguezf@outlook.com>
2015-05-22 14:50:05 -0500

    Description Of Tables

---
+++ DescriptionOfTables.sql
@@ -0,0 +1,11 @@
+USE [DATABASE]
+GO
+
+SELECT distinct s.name as [SCHEMA_TABLE], 
+	T.NAME AS [TABLE NAME], 
+	ISNULL(sep.value, '') [Description]
+FROM SYS.OBJECTS AS T
+	JOIN SYS.schemas as S ON T.schema_id = S.schema_id
+	LEFT JOIN sys.extended_properties sep on T.OBJECT_ID = sep.major_id AND sep.minor_id = 0 AND sep.name = 'MS_Description'
+WHERE T.TYPE_DESC='USER_TABLE' and s.name <> 'dbo' 
+ORDER BY s.name, T.NAME;
