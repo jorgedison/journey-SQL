60fe564e8c722ea4607db69b4b7e02123b06e1cb
Jorge Rodriguez Flores <jrodriguezf@outlook.com>
2015-07-02 17:28:22 -0500

    LogAuditoriaBD

---
+++ Log Auditoria/LogAuditoriaFromOtheDB.sql
@@ -0,0 +1,75 @@
+USE [master]
+GO
+
+/*	Script: Creacion de BD de auditoria a otras BD (SP)
+	Autor: Jorge Rodriguez
+*/
+
+/*1. Crea BD de auditoria*/
+CREATE DATABASE AuditDB;
+GO
+USE AuditDB;
+GO
+
+/*2. Crea Tabla de auditoria*/
+CREATE TABLE dbo.DDLEvents
+(
+    EventDate    DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    EventType    NVARCHAR(64),
+    EventDDL     NVARCHAR(MAX),
+    EventXML     XML,
+    DatabaseName NVARCHAR(255),
+    SchemaName   NVARCHAR(255),
+    ObjectName   NVARCHAR(255),
+    HostName     VARCHAR(64),
+    IPAddress    VARCHAR(32),
+    ProgramName  NVARCHAR(255),
+    LoginName    NVARCHAR(255)
+);
+
+/*2. Crea Trigger de auditoria*/
+USE YourDatabase;
+GO
+CREATE TRIGGER DDLTriggerName
+    ON DATABASE
+    FOR CREATE_PROCEDURE, ALTER_PROCEDURE, DROP_PROCEDURE
+AS
+BEGIN
+    SET NOCOUNT ON;
+    DECLARE
+        @EventData XML = EVENTDATA();
+ 
+    DECLARE 
+        @ip VARCHAR(32) =
+        (
+            SELECT client_net_address
+                FROM sys.dm_exec_connections
+                WHERE session_id = @@SPID
+        );
+ 
+    INSERT AuditDB.dbo.DDLEvents
+    (
+        EventType,
+        EventDDL,
+        EventXML,
+        DatabaseName,
+        SchemaName,
+        ObjectName,
+        HostName,
+        IPAddress,
+        ProgramName,
+        LoginName
+    )
+    SELECT
+        @EventData.value('(/EVENT_INSTANCE/EventType)[1]',   'NVARCHAR(100)'), 
+        @EventData.value('(/EVENT_INSTANCE/TSQLCommand)[1]', 'NVARCHAR(MAX)'),
+        @EventData,
+        DB_NAME(),
+        @EventData.value('(/EVENT_INSTANCE/SchemaName)[1]',  'NVARCHAR(255)'), 
+        @EventData.value('(/EVENT_INSTANCE/ObjectName)[1]',  'NVARCHAR(255)'),
+        HOST_NAME(),
+        @ip,
+        PROGRAM_NAME(),
+        SUSER_SNAME();
+END
+GO
