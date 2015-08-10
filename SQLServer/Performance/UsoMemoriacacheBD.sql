e53155a75b791f4673bf14955babcd98df533ca1
Jorge Rodriguez Flores <jrodriguezf@outlook.com>
2015-08-10 11:24:22 -0500

    Uso alternativo de script de modificación de cache BD

--- SQLServer/Performance/UsoMemoriacacheBD.sql
+++ SQLServer/Performance/UsoMemoriacacheBD.sql
@@ -11,3 +11,18 @@ SELECT ROW_NUMBER() OVER(ORDER BY CachedSize DESC) AS [Buffer Pool Rank], [Datab
        CAST(CachedSize / SUM(CachedSize) OVER() * 100.0 AS DECIMAL(5,2)) AS [Buffer Pool Percent]
 FROM AggregateBufferPoolUsage
 ORDER BY [Buffer Pool Rank] OPTION (RECOMPILE);
+
+-- SCRIPT ALTERNATIVO, GUARDA EN UNA TABLA
+
+WITH AggregateBufferPoolUsage
+AS
+(SELECT DB_NAME(database_id) AS [Database Name], CAST(COUNT(*) * 8/1024.0 AS DECIMAL (10,2))  AS [CachedSize]
+FROM sys.dm_os_buffer_descriptors WITH (NOLOCK)
+WHERE database_id <> 32767 -- ResourceDB
+GROUP BY DB_NAME(database_id))
+INSERT INTO [AuditDB].[dbo].[DBMonitor] 
+		([Buffer Pool Rank],[Database Name],[Cached Size (MB)],[Buffer Pool Percent], [Fecha])
+SELECT ROW_NUMBER() OVER(ORDER BY CachedSize DESC) AS [Buffer Pool Rank], [Database Name], CachedSize AS [Cached Size (MB)],
+       CAST(CachedSize / SUM(CachedSize) OVER() * 100.0 AS DECIMAL(5,2)) AS [Buffer Pool Percent], getdate()
+FROM AggregateBufferPoolUsage
+ORDER BY [Buffer Pool Rank] OPTION (RECOMPILE);
