02b5871c62ce426e4e4e3048dbc231b0eeaebecd
Jorge Rodriguez Flores <jrodriguezf@outlook.com>
2015-09-09 11:21:27 -0500

    Tamaño Tablas y numero de registros

--- SQLServer/TamañoTablas.sql
+++ SQLServer/TamañoTablas.sql
@@ -2,7 +2,7 @@ USE [DATABASE]
 GO
 
 SELECT 
-    X.[name], 
+    X.[name],
     REPLACE(CONVERT(varchar, CONVERT(money, X.[rows]), 1), '.00', '') 
         AS [rows], 
     REPLACE(CONVERT(varchar, CONVERT(money, X.[reserved]), 1), '.00', '') 
@@ -12,7 +12,9 @@ SELECT
     REPLACE(CONVERT(varchar, CONVERT(money, X.[index_size]), 1), '.00', '') 
         AS [index_size], 
     REPLACE(CONVERT(varchar, CONVERT(money, X.[unused]), 1), '.00', '') 
-        AS [unused] 
+        AS [unused], 
+	X.MB,
+	GETDATE() AS [fecha]
 FROM 
 (SELECT 
     CAST(object_name(id) AS varchar(50)) 
@@ -26,11 +28,14 @@ FROM
     SUM(CONVERT(bigint, used) - CONVERT(bigint, dpages)) * 8 
         AS index_size, 
     SUM(CONVERT(bigint, reserved) - CONVERT(bigint, used)) * 8 
-        AS unused 
-    FROM sysindexes WITH (NOLOCK) 
+        AS unused,
+	SUM(reserved_page_count)*8.0/1024 + SUM(lob_reserved_page_count)*8.0/1024 as MB,
+	GETDATE() as hora
+    FROM sysindexes WITH (NOLOCK),  sys.dm_db_partition_stats WITH (NOLOCK)
     WHERE sysindexes.indid IN (0, 1, 255) 
         AND sysindexes.id > 100 
         AND object_name(sysindexes.id) <> 'dtproperties' 
+		AND sys.dm_db_partition_stats.object_id=sysindexes.id
     GROUP BY sysindexes.id WITH ROLLUP
 ) AS X
 WHERE X.[name] is not null
