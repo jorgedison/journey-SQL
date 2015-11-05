2a7611c8ee1443012771b22116c89b2ec1d20a9f
Jorge Rodriguez Flores <jrodriguezf@outlook.com>
2015-11-05 15:36:00 -0500

    Version Base de datos

---
+++ PostreSQL/db_version.sql
@@ -0,0 +1,7 @@
+﻿--Database Type	PostgreSQL
+--Script Location	Basic Administration
+--Script Type	SQL File
+--Script Name	db_version
+--Script Description	Returns the version of PostgreSQL that is runnin
+
+SELECT VERSION()
