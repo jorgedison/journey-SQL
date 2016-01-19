f99fd5716f4123dd635fc6a2bfe7d7d468ba9f5b
Jorge Rodriguez Flores <jrodriguezf@outlook.com>
2016-01-19 16:37:35 -0500

    Alternativa cambia collation de campos

--- SQLServer/Formatos/CambiaCollationCampo.sql
+++ SQLServer/Formatos/CambiaCollationCampo.sql
@@ -23,3 +23,39 @@ SELECT 'ALTER TABLE [' + SYS.OBJECTS.NAME + '] ALTER COLUMN ['
     AND SYS.COLUMNS.COLLATION_NAME IS NOT NULL
     AND NOT ( SYS.OBJECTS.NAME LIKE 'SYS%' )
     AND NOT ( SYS.TYPES.NAME LIKE 'SYS%' )
+
+-- CONSULTA ALTERNATIVA
+
+USE DATABASE
+GO
+
+DECLARE @collate SYSNAME
+SELECT @collate = 'x'
+
+SELECT 
+      '[' + SCHEMA_NAME(o.[schema_id]) + '].[' + o.name + '] -> ' + c.name
+    , 'ALTER TABLE [' + SCHEMA_NAME(o.[schema_id]) + '].[' + o.name + ']
+        ALTER COLUMN [' + c.name + '] ' +
+        UPPER(t.name) + 
+        CASE WHEN t.name NOT IN ('ntext', 'text') 
+            THEN '(' + 
+                CASE 
+                    WHEN t.name IN ('nchar', 'nvarchar') AND c.max_length != -1 
+                        THEN CAST(c.max_length / 2 AS VARCHAR(10))
+                    WHEN t.name IN ('nchar', 'nvarchar') AND c.max_length = -1 
+                        THEN 'MAX'
+                    ELSE CAST(c.max_length AS VARCHAR(10)) 
+                END + ')' 
+            ELSE '' 
+        END + ' COLLATE ' + @collate + 
+        CASE WHEN c.is_nullable = 1 
+            THEN ' NULL'
+            ELSE ' NOT NULL'
+        END AS T
+FROM sys.columns c WITH(NOLOCK)
+JOIN sys.objects o WITH(NOLOCK) ON c.[object_id] = o.[object_id]
+JOIN sys.types t WITH(NOLOCK) ON c.system_type_id = t.system_type_id AND c.user_type_id = t.user_type_id
+WHERE t.name IN ('char', 'varchar', 'text', 'nvarchar', 'ntext', 'nchar')
+    AND c.collation_name != @collate
+    AND o.[type] = 'U'
+	ORDER BY T ASC
