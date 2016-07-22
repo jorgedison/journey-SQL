67af60638b31d7ef4f3447e67ad715be1bdcc01a
Jorge Rodriguez Flores <jrodriguezf@outlook.com>
2016-07-22 14:49:01 -0500

    Create DescriptionTable.sql

---
+++ PostreSQL/Administracion Basica/DescriptionTable.sql
@@ -0,0 +1,17 @@
+SELECT c.relname As tname, CASE WHEN c.relkind = 'v' THEN 'view' ELSE 'table' END As type, 
+    pg_get_userbyid(c.relowner) AS towner, t.spcname AS tspace, 
+    n.nspname AS sname,  d.description
+   FROM pg_class As c
+   LEFT JOIN pg_namespace n ON n.oid = c.relnamespace
+   LEFT JOIN pg_tablespace t ON t.oid = c.reltablespace
+   LEFT JOIN pg_description As d ON (d.objoid = c.oid )
+   WHERE c.relkind IN('r', 'v') --AND d.description > ''
+   AND pg_get_userbyid(c.relowner) = 'DATABASE'
+   ORDER BY n.nspname, c.relname;
+   
+  
+SELECT * from pg_class c WHERE c.relkind IN('r', 'v') AND pg_get_userbyid(c.relowner) = 'DATABASE'
+
+SELECT * from pg_class WHERE table_catalog='DATABASE' and table_type='BASE TABLE' and table_schema='public';
+
+SELECT * from information_schema.tables WHERE table_catalog='DATABASE' and table_type='BASE TABLE' and table_schema='public'
