03fdbc67c1b5646357b43f5836996a6d721bc225
Jorge Rodriguez Flores <jrodriguezf@outlook.com>
2016-08-18 15:40:21 -0500

    Agrega Lista de columnas

--- PostreSQL/Administracion Basica/FieldDescription.sql
+++ PostreSQL/Administracion Basica/FieldDescription.sql
@@ -1,3 +1,13 @@
+-- Lista de columnas
+
+SELECT *
+FROM information_schema.columns
+WHERE table_schema = 'public'
+ -- AND table_name   = 'your_table'
+order by table_name asc
+
+-- Lista de columnas y descripciones
+
 SELECT
 cols.column_name,
 pg_catalog.col_description(c.oid, cols.ordinal_position::int), *
