08e623c77e7b904b26ca8b0448bf508df8dd8286
Jorge Rodriguez Flores <jrodriguezf@outlook.com>
2017-02-08 15:20:33 -0500

    Update FieldDescription.sql

--- PostreSQL/Administracion Basica/FieldDescription.sql
+++ PostreSQL/Administracion Basica/FieldDescription.sql
@@ -16,3 +16,21 @@ WHERE
 cols.table_catalog = 'Databasename' and
 cols.table_schema = 'public' and
 cols.table_name = c.relname
+
+-- Lista columnas, descripcion, tipos de datos
+
+SELECT *,
+ isc.table_name,
+ isc.ordinal_position::integer AS ordinal_position,
+ isc.column_name::character varying AS column_name,
+ isc.column_default::character varying AS column_default,
+ isc.data_type::character varying AS data_type,
+ isc.character_maximum_length::integer AS str_length,
+        CASE
+            WHEN isc.udt_name::text = 'int4'::text OR isc.udt_name::text = 
+'bool'::text THEN isc.data_type::character varying
+            ELSE isc.udt_name::character varying
+        END AS udt_name
+   FROM information_schema.columns isc
+  WHERE isc.table_schema::text = 'public'::text --and isc.table_name='systemformcontrols'
+  ORDER BY isc.table_name, isc.ordinal_position;
