052493d7fffe427e92c8b6009c825c94b2bd0383
Jorge Rodriguez Flores <jrodriguezf@outlook.com>
2017-09-05 18:45:14 -0500

    Update DescriptionFunction.sql

--- PostreSQL/Administracion Basica/DescriptionFunction.sql
+++ PostreSQL/Administracion Basica/DescriptionFunction.sql
@@ -15,3 +15,14 @@ INNER JOIN pg_catalog.pg_description c ON c.objoid=p.oid
 WHERE  n.nspname !~~ 'pg_%'
 AND    n.nspname <> 'information_schema'
 AND (c.description IS NOT NULL OR c.description IS NULL)
+
+-- Lista de funciones y parametros
+
+SELECT routines.routine_name, 
+	parameters.data_type, 
+	parameters.ordinal_position 
+FROM information_schema.routines 
+JOIN information_schema.parameters ON routines.specific_name=parameters.specific_name 
+WHERE routines.specific_schema='public'
+-- and routine_name='nombre_funcion' 
+ORDER BY routines.routine_name, parameters.ordinal_position;
