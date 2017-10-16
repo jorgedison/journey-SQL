3b96e8f0e865f8c11019e72ad353dfd437492fdf
Jorge Rodriguez Flores <jrodriguezf@outlook.com>
2017-10-16 14:34:18 -0500

    Update secuencias.sql

--- PostreSQL/Administracion Basica/secuencias.sql
+++ PostreSQL/Administracion Basica/secuencias.sql
@@ -7,3 +7,20 @@ SELECT nextval('application_seq');
 SELECT setval('application_seq', (SELECT MAX(iapplicationid) FROM application));
  
 SELECT setval('application_seq', COALESCE((SELECT MAX(iapplicationid)+1 FROM application), 1), false);
+
+-- Funcion get_sequence_last_value
+
+CREATE FUNCTION public.get_sequence_last_value(name) RETURNS int4 AS '
+DECLARE
+  ls_sequence ALIAS FOR $1;
+  lr_record RECORD;
+  li_return INT4;
+BEGIN
+  FOR lr_record IN EXECUTE ''SELECT last_value FROM '' || ls_sequence LOOP
+    li_return := lr_record.last_value;
+  END LOOP;
+  RETURN li_return;
+END;'  LANGUAGE 'plpgsql' VOLATILE;
+
+SELECT c.relname, get_sequence_last_value(c.relname)
+FROM pg_class c WHERE (c.relkind = 'S');
