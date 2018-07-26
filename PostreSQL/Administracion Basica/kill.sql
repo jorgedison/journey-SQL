6b47c0ab5514caa3d009fe5d90c10fbbeb1a5742
Jorge Rodriguez Flores <jrodriguezf@outlook.com>
2018-07-26 11:02:34 -0500

    Elimina todas las sesiones de la base de datos

--- PostreSQL/Administracion Basica/kill.sql
+++ PostreSQL/Administracion Basica/kill.sql
@@ -33,3 +33,10 @@ WHERE
     select count(*) from pg_stat_activity;
     select * from pg_stat_activity; where 
 select now()
+
+-- Elimina todas las sesiones de la base de datos
+
+SELECT pg_terminate_backend(pg_stat_activity.pid)
+FROM pg_stat_activity
+WHERE pg_stat_activity.datname = 'TARGET_DB' ← change this to your DB
+  AND pid <> pg_backend_pid();
