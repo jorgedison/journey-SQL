adfc14fc15d55f163aa2640971cbcf1811e74c09
Jorge Rodriguez Flores <jorgeedison1@gmail.com>
2019-06-14 02:04:56 -0500

    Cantidad de registros por tablas MsSQL

--- SQLServer/TamañoTablas.sql
+++ SQLServer/TamañoTablas.sql
@@ -78,3 +78,15 @@ FROM sysindexes AS i
   INNER JOIN sysobjects AS o ON i.id = o.id 
 WHERE i.indid < 2  AND OBJECTPROPERTY(o.id, 'IsMSShipped') = 0
 ORDER BY I.rowcnt DESC
+
+-- Tamaño de tablas (4)
+
+CREATE TABLE #counts
+(
+    table_name varchar(255),
+    row_count int
+)
+
+EXEC sp_MSForEachTable @command1='INSERT #counts (table_name, row_count) SELECT ''?'', COUNT(*) FROM ?'
+SELECT table_name, row_count FROM #counts ORDER BY row_count DESC
+DROP TABLE #counts
