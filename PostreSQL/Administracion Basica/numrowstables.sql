97576be8c047f2514baa2f7e198812b7dda8273d
Jorge Rodriguez Flores <jorgeedison1@gmail.com>
2019-07-31 11:11:28 -0500

    Update numrowstables.sql

--- PostreSQL/Administracion Basica/numrowstables.sql
+++ PostreSQL/Administracion Basica/numrowstables.sql
@@ -1,3 +1,12 @@
 -- Numero de registros por tabla de una base de datos
 
 SELECT schemaname,relname,n_live_tup  FROM pg_stat_user_tables order by 2 asc;
+
+SELECT 
+  nspname AS schemaname,relname,reltuples
+FROM pg_class C
+LEFT JOIN pg_namespace N ON (N.oid = C.relnamespace)
+WHERE 
+  nspname NOT IN ('pg_catalog', 'information_schema') AND
+  relkind='r' 
+ORDER BY reltuples DESC;
