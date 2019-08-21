ede50f97db0c7cb97cdecec1abbc08117108942d
Jorge Rodriguez Flores <jorgeedison1@gmail.com>
2019-08-21 16:20:38 -0500

    Update DataDummy.sql

--- PostreSQL/Administracion Basica/DataDummy.sql
+++ PostreSQL/Administracion Basica/DataDummy.sql
@@ -1,6 +1,7 @@
 	SELECT 'HTWWWW-' ||generate_series(10000,96000), 'SSSSSSS-' ||generate_series(10000,96000), 
   generate_series(10000,96000), generate_series(10000,96000), 
 			now(), now(), 
-			generate_series(10000,96000), 'MEMDDDD-' ||generate_series(10000,96000), 'HDDDDDT-' ||generate_series(10000,96000), 
+			generate_series(10000,96000), 'MEMDDDD-' ||generate_series(10000,96000), 
+			'HDDDDDT-' ||generate_series(10000,96000), 
       'HT44444-' ||generate_series(10000,96000), 'HT4444-' ||generate_series(10000,96000);
 			
