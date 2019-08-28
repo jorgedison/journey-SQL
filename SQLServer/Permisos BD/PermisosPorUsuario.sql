6fd0ee80a7936ceecd6ddbf5cff03537ff6d573e
Jorge Rodriguez Flores <jorgeedison1@gmail.com>
2019-08-28 17:06:11 -0500

    Update PermisosPorUsuario.sql

--- SQLServer/Permisos BD/PermisosPorUsuario.sql
+++ SQLServer/Permisos BD/PermisosPorUsuario.sql
@@ -1,25 +1,26 @@
-# Permisos a Procedimientos almacenados
+DECLARE @alias NVARCHAR(max)
+set @alias = 'jrodriguez' 
+
+-- Permisos a Procedimientos almacenados
 
 SELECT
-'GRANT VIEW DEFINITION ON [' +SPECIFIC_SCHEMA + '].['+ SPECIFIC_NAME +'] to [jrodriguez];'
+'GRANT VIEW DEFINITION ON [' +SPECIFIC_SCHEMA + '].['+ SPECIFIC_NAME +'] to ['+@alias+'];'
    from information_schema.routines 
  where routine_type = 'PROCEDURE'
-GO
 
 SELECT
-'DENY ALTER ON [' +SPECIFIC_SCHEMA + '].['+ SPECIFIC_NAME +'] to [jrodriguez];'
+'DENY ALTER ON [' +SPECIFIC_SCHEMA + '].['+ SPECIFIC_NAME +'] to ['+@alias+'];'
    from information_schema.routines 
  where routine_type = 'PROCEDURE'
-GO
 
-# Permisos a vistas
+-- Permisos a vistas
 
 SELECT
-'GRANT VIEW DEFINITION ON [' +TABLE_SCHEMA + '].['+ TABLE_NAME +'] to [jrodriguez];'
+'GRANT VIEW DEFINITION ON [' +TABLE_SCHEMA + '].['+ TABLE_NAME +'] to ['+@alias+'];'
    from information_schema.VIEWS 
-GO
+
 
 SELECT
-'DENY ALTER ON [' +TABLE_SCHEMA + '].['+ TABLE_NAME +'] to [jrodriguez];'
+'DENY ALTER ON [' +TABLE_SCHEMA + '].['+ TABLE_NAME +'] to ['+@alias+'];'
    from information_schema.VIEWS 
 GO
