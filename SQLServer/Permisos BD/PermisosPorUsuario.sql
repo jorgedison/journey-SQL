37558f1811937fe8bfb0c67badf6c71b7d668982
Jorge Rodriguez Flores <jorgeedison1@gmail.com>
2019-12-12 11:41:32 -0500

    Permisos a tablas y funciones

--- SQLServer/Permisos BD/PermisosPorUsuario.sql
+++ SQLServer/Permisos BD/PermisosPorUsuario.sql
@@ -1,6 +1,8 @@
 DECLARE @alias NVARCHAR(max)
 set @alias = 'jrodriguez' 
 
+SET NOCOUNT ON
+
 -- Permisos a Procedimientos almacenados
 
 SELECT
@@ -23,4 +25,28 @@ SELECT
 SELECT
 'DENY ALTER ON [' +TABLE_SCHEMA + '].['+ TABLE_NAME +'] to ['+@alias+'];'
    from information_schema.VIEWS 
-GO
+
+
+-- Permisos a Funciones
+
+SELECT
+'GRANT VIEW DEFINITION ON [' +SPECIFIC_SCHEMA + '].['+ SPECIFIC_NAME +'] to ['+@alias+'];'
+   from information_schema.routines 
+ where routine_type = 'FUNCTION'
+
+SELECT
+'DENY ALTER ON [' +SPECIFIC_SCHEMA + '].['+ SPECIFIC_NAME +'] to ['+@alias+'];'
+   from information_schema.routines 
+ where routine_type = 'FUNCTION'
+
+ -- Permisos a Tablas
+
+ SELECT
+'GRANT VIEW DEFINITION ON [' +TABLE_SCHEMA + '].['+ TABLE_NAME +'] to ['+@alias+'];'
+   from INFORMATION_SCHEMA.TABLES
+ where TABLE_TYPE = 'BASE TABLE'
+
+ SELECT
+'DENY ALTER ON [' +TABLE_SCHEMA + '].['+ TABLE_NAME +'] to ['+@alias+'];'
+   from INFORMATION_SCHEMA.TABLES
+ where TABLE_TYPE = 'BASE TABLE'
