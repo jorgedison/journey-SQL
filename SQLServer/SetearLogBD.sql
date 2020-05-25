f93bde0d2b82f3e428a06906d66087b6b123c16c
Jorge Rodriguez Flores <jorgeedison1@gmail.com>
2020-05-25 09:40:09 -0500

    SHRINKFILE All Database

--- SQLServer/SetearLogBD.sql
+++ SQLServer/SetearLogBD.sql
@@ -20,3 +20,16 @@ GO
 ALTER DATABASE [DATABASE]
 SET RECOVERY FULL;
 GO
+
+
+----------------------------
+
+SELECT 
+      'USE [' + d.name + N']' + CHAR(13) + CHAR(10) 
+    + 'DBCC SHRINKFILE (N''' + mf.name + N''' , 0, TRUNCATEONLY);' 
+    + CHAR(13) + CHAR(10) + CHAR(13) + CHAR(10) 
+FROM 
+         sys.master_files mf 
+    JOIN sys.databases d 
+        ON mf.database_id = d.database_id 
+WHERE d.database_id > 4;
