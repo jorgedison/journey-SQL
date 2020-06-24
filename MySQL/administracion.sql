7e49507363fe16ca626fa883acc000d3d4e44b76
Jorge Rodriguez Flores <jorgeedison1@gmail.com>
2020-06-24 15:35:13 -0500

    Create administracion.sql

---
+++ MySQL/administracion.sql
@@ -0,0 +1,11 @@
+USE Databasename − This will be used to select a database in the MySQL workarea.
+
+SHOW DATABASES − Lists out the databases that are accessible by the MySQL DBMS.
+
+SHOW TABLES − Shows the tables in the database once a database has been selected with the use command.
+
+SHOW COLUMNS FROM tablename: Shows the attributes, types of attributes, key information, whether NULL is permitted, defaults, and other information for a table.
+
+SHOW INDEX FROM tablename − Presents the details of all indexes on the table, including the PRIMARY KEY.
+
+SHOW TABLE STATUS LIKE tablename\G − Reports details of the MySQL DBMS performance and statistics.
