6af421e37b9a746edadb4220940d9c0867750051
Jorge Rodriguez Flores <jorgeedison1@gmail.com>
2020-07-26 15:20:30 -0500

    Update blocking.sql

--- PostreSQL/Administracion Basica/blocking.sql
+++ PostreSQL/Administracion Basica/blocking.sql
@@ -15,3 +15,11 @@ FROM PG_CATALOG.PG_LOCKS BL
 		ON KL.PID = KA.PID
 		ON BL.TRANSACTIONID = KL.TRANSACTIONID AND BL.PID != KL.PID
 WHERE NOT BL.GRANTED;
+
+
+select pid, 
+       usename, 
+       pg_blocking_pids(pid) as blocked_by, 
+       query as blocked_query
+from pg_stat_activity
+where cardinality(pg_blocking_pids(pid)) > 0;
