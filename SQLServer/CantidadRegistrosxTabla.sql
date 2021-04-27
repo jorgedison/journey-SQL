3a82de9a03290c973c5c5f2eebe4fccfe95fd87a
Jorge Rodriguez Flores <jorgeedison1@gmail.com>
2021-04-27 15:19:14 -0500

    Update CantidadRegistrosxTabla.sql

--- SQLServer/CantidadRegistrosxTabla.sql
+++ SQLServer/CantidadRegistrosxTabla.sql
@@ -50,3 +50,21 @@ GROUP BY
       , sOBJ.name
 ORDER BY 2 desc
 GO
+
+-- Cantidad de registros por tabla y esquema (3)
+
+DECLARE @QueryString NVARCHAR(MAX) ;
+SELECT @QueryString = COALESCE(@QueryString + ' UNION ALL ','')
+                      + 'SELECT '
+                      + '''' + QUOTENAME(SCHEMA_NAME(sOBJ.schema_id))
+                      + '.' + QUOTENAME(sOBJ.name) + '''' + ' AS [TableName]
+                      , COUNT(*) AS [RowCount] FROM '
+                      + QUOTENAME(SCHEMA_NAME(sOBJ.schema_id))
+                      + '.' + QUOTENAME(sOBJ.name) + ' WITH (NOLOCK) '
+FROM sys.objects AS sOBJ
+WHERE
+      sOBJ.type = 'U'
+      AND sOBJ.is_ms_shipped = 0x0
+ORDER BY SCHEMA_NAME(sOBJ.schema_id), sOBJ.name ;
+EXEC sp_executesql @QueryString
+GO
