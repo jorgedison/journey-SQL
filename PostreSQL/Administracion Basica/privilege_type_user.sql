57bd2cc82897653936ed4f50721d231155ff69a0
Jorge Rodriguez Flores <jorgeedison1@gmail.com>
2022-09-07 10:42:00 -0500

    Create privilege_type_user.sql

---
+++ PostreSQL/Administracion Basica/privilege_type_user.sql
@@ -0,0 +1,17 @@
+SELECT grantee, privilege_type 
+FROM information_schema.role_table_grants 
+WHERE table_name='mytable'
+
+
+SELECT r.rolname, r.rolsuper, r.rolinherit,
+  r.rolcreaterole, r.rolcreatedb, r.rolcanlogin,
+  r.rolconnlimit, r.rolvaliduntil,
+  ARRAY(SELECT b.rolname
+        FROM pg_catalog.pg_auth_members m
+        JOIN pg_catalog.pg_roles b ON (m.roleid = b.oid)
+        WHERE m.member = r.oid) as memberof
+, r.rolreplication
+, r.rolbypassrls
+FROM pg_catalog.pg_roles r
+WHERE r.rolname !~ '^pg_'
+ORDER BY 1;
